ac56096dfcdadab77706de5c4e79c9370f70e18d
wuli133144 <1683358846@qq.com>
2017-11-13 11:48:09 +0800

    添加了新的测试案列

--- main.c
+++ main.c
@@ -7,17 +7,43 @@
 typedef struct person
 {
      int age;
-     TAILQ_ENTRY(person) *next;
+     TAILQ_ENTRY(person) next;
 }t_person;
 
 
+
+          
 
 int main()
 {
     
     TAILQ_HEAD(person_set,person)head=TAILQ_HEAD_INITIALIZER(head);
      t_person * p=(t_person *)malloc(sizeof(t_person));
+     p->age=1;
+     
+     t_person * p1=(t_person *)malloc(sizeof(t_person));
+     p1->age=2;
+
+      t_person * p2=(t_person *)malloc(sizeof(t_person));
+     p2->age=2;
+
      if(!p){printf("error"); exit(0);}
      TAILQ_INSERT_HEAD(&head,p,next);
+     TAILQ_INSERT_HEAD(&head,p1,next);
+     TAILQ_INSERT_HEAD(&head,p2,next);
+
+
+
+
+
+
+     //TAILQ_INSERT_HEAD(&head,p,next);
+     //TAILQ_INSERT_HEAD(&head,p,next);
+
+    t_person *pdata=NULL;
+     TAILQ_FOREACH(pdata,&head,next){
+         printf("data=%d",pdata->age);
+     }
+
     return 0;
 }
