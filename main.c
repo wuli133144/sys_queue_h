7d3c93d0d2a4bec2f43dab3eb38b7662fc8eb8c4
wuli133144 <1683358846@qq.com>
2017-11-13 11:59:12 +0800

    version 2

--- main.c
+++ main.c
@@ -26,21 +26,25 @@ int main()
 
       t_person * p2=(t_person *)malloc(sizeof(t_person));
      p2->age=2;
-
+  
      if(!p){printf("error"); exit(0);}
+     //头插入法
      TAILQ_INSERT_HEAD(&head,p,next);
      TAILQ_INSERT_HEAD(&head,p1,next);
      TAILQ_INSERT_HEAD(&head,p2,next);
+      
+ 
 
-
-
-
+     //尾插入法
+     t_person *p4=(t_person *)malloc(sizeof(t_person));
+     TAILQ_INSERT_TAIL(&head,p4,next);
 
 
      //TAILQ_INSERT_HEAD(&head,p,next);
      //TAILQ_INSERT_HEAD(&head,p,next);
 
     t_person *pdata=NULL;
+    //遍历
      TAILQ_FOREACH(pdata,&head,next){
          printf("data=%d",pdata->age);
      }
